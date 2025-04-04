8c964e3a16a403f36f8ed69262ac443c0a65e6c9
Aung Nyein Khant <139859864+AungNyeinKhant@users.noreply.github.com>
2025-04-04 17:21:09 +0700

    before-register-modify

--- src/components/cards/ArtistCard.tsx
+++ src/components/cards/ArtistCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 type Artist = {
   id: string;
-  title: string;
+  name: string;
   image: string;
   listen_count?: number;
 };
@@ -26,13 +26,13 @@ const ArtistCard: FC<ArtistCardProps> = ({ artist, size = "medium" }) => {
       >
         <img
           src={artist.image}
-          alt={artist.title}
+          alt={artist.name}
           className='w-full h-full object-cover transform transition-transform group-hover:scale-105'
         />
       </div>
       <div className='mt-3 text-center'>
         <h3 className='text-primaryText font-semibold truncate'>
-          {artist.title}
+          {artist.name}
         </h3>
         {artist.listen_count && (
           <p className='text-gray-400 text-sm truncate'>
