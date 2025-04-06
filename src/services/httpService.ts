62974519f0083eb1ac4e0ef96f0565a46cc70c2b
Aung Nyein Khant <139859864+AungNyeinKhant@users.noreply.github.com>
2025-04-06 15:32:34 +0700

    0.7.8.3-redirect-no-packge-user-fix

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-let navigateFunction: (path: string) => void = () => {
-  console.warn(
-    "Navigation handler not yet set. Ensure useNavigationSetup is called in a component."
-  );
-};
-
-export const setNavigationHandler = (handler: (path: string) => void) => {
-  navigateFunction = handler;
-};
-
-export const navigateTo = (path: string) => {
-  navigateFunction(path);
-};
-
 export const userAPI = axios.create({
   baseURL: "http://localhost:3000/api/user",
   headers: {
